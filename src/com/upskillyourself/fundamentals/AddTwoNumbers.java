a2929a93d0aac30fe3e7ddb642b7118269e17305
Subramanian1503 <roopan1503@gmail.com>
2023-12-20 11:45:09 +0530

    [JAVA] AddTwoNumbers
    - to understand variables and datatypes

---
+++ src/com/upskillyourself/fundamentals/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.upskillyourself.fundamentals;
+
+public class AddTwoNumbers {
+
+    public static void main(String args[]){
+        // Initialise a, b
+        int a = 10;
+        int b = 20;
+        // Add a and b and store it in c
+        int c = a + b;
+        // print c
+        System.out.println(c);
+    }
+}
